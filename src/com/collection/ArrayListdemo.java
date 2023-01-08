393e8ab6c5a4c88d69e093ef544b28b2488cceab
akshaygadhave <gadhaveakshay8@gmail.com>
2023-01-08 15:57:22 +0530

    Array List iterator

--- src/com/collection/ArrayListdemo.java
+++ src/com/collection/ArrayListdemo.java
@@ -1,23 +1,29 @@
 package com.collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListdemo {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<String> arrayList = new ArrayList<String>();
 		arrayList.add("Akshay");
 		arrayList.add("Manisha");
 		arrayList.add("Rohan");
 		arrayList.add("Nikhil");
-		
-		//System.out.println(arrayList);
-		
-		for(String str: arrayList)
-		{
-			System.out.println(str);
+
+		// System.out.println(arrayList);
+
+		/*
+		 * for(String str: arrayList) { System.out.println(str); }
+		 */
+
+		Iterator<String> itr = arrayList.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+			
 		}
 	}
-	
+
 }
