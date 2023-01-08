de000824c0ec40bed7f96cfa7b4576b885d5c013
akshaygadhave <gadhaveakshay8@gmail.com>
2023-01-08 15:35:31 +0530

    first commit

---
+++ src/com/collection/ArrayListdemo.java
@@ -0,0 +1,18 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListdemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add("Akshay");
+		arrayList.add("Manisha");
+		arrayList.add("Rohan");
+		arrayList.add("Nikhil");
+		
+		System.out.println(arrayList);
+	}
+	
+}
---
+++ src/com/collection/LinkedListDemo.java
@@ -0,0 +1,20 @@
+package com.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = new LinkedList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		
+		System.out.println(list);
+		
+	}
+
+}
